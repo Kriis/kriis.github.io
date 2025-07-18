eabb8379cccf6f6e21cf0948017b02074945dbee
Hai Anh Le <HaiAnh.Le@rockwellautomation.com>
2025-07-18 09:26:55 +0800

    change banner

--- renderer.js
+++ renderer.js
@@ -7,16 +7,23 @@ terminal.setAttribute("class", "terminal")
 terminal.setAttribute("contenteditable", "true")
 terminal.setAttribute("tabindex", "0")
 
+
 function printBanner(terminal) {
 	terminal.innerHTML = `<div id="filterBar" style="display:none"></div>
-	<p contenteditable="false">███████╗&nbsp█████╗&nbsp███████╗██╗&nbsp&nbsp&nbsp██╗</p>
-	<p contenteditable="false">██╔════╝██╔══██╗██╔════╝╚██╗&nbsp██╔╝</p>
-	<p contenteditable="false">█████╗&nbsp&nbsp███████║███████╗&nbsp╚████╔╝&nbsp</p>
-	<p contenteditable="false">██╔══╝&nbsp&nbsp██╔══██║╚════██║&nbsp&nbsp╚██╔╝&nbsp&nbsp</p>
-	<p contenteditable="false">███████╗██║&nbsp&nbsp██║███████║&nbsp&nbsp&nbsp██║&nbsp&nbsp&nbsp</p>
-	<p contenteditable="false">╚══════╝╚═╝&nbsp&nbsp╚═╝╚══════╝&nbsp&nbsp&nbsp╚═╝&nbsp&nbsp&nbsp</p>
+	<p contenteditable="false">&nbsp█████╗&nbsp███╗&nbsp&nbsp&nbsp██╗██╗&nbsp&nbsp██╗██╗&nbsp&nbsp&nbsp&nbsp&nbsp███████╗&nbsp&nbsp&nbsp&nbsp██████╗&nbsp██████╗&nbsp&nbsp██████╗&nbsp███████╗██╗██╗&nbsp&nbsp&nbsp&nbsp&nbsp███████╗</p>
+	<p contenteditable="false">██╔══██╗████╗&nbsp&nbsp██║██║&nbsp&nbsp██║██║&nbsp&nbsp&nbsp&nbsp&nbsp██╔════╝&nbsp&nbsp&nbsp&nbsp██╔══██╗██╔══██╗██╔═══██╗██╔════╝██║██║&nbsp&nbsp&nbsp&nbsp&nbsp██╔════╝</p>
+	<p contenteditable="false">███████║██╔██╗&nbsp██║███████║██║&nbsp&nbsp&nbsp&nbsp&nbsp█████╗&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp██████╔╝██████╔╝██║&nbsp&nbsp&nbsp██║█████╗&nbsp&nbsp██║██║&nbsp&nbsp&nbsp&nbsp&nbsp█████╗&nbsp&nbsp</p>
+	<p contenteditable="false">██╔══██║██║╚██╗██║██╔══██║██║&nbsp&nbsp&nbsp&nbsp&nbsp██╔══╝&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp██╔═══╝&nbsp██╔══██╗██║&nbsp&nbsp&nbsp██║██╔══╝&nbsp&nbsp██║██║&nbsp&nbsp&nbsp&nbsp&nbsp██╔══╝&nbsp&nbsp</p>
+	<p contenteditable="false">██║&nbsp&nbsp██║██║&nbsp╚████║██║&nbsp&nbsp██║███████╗███████╗&nbsp&nbsp&nbsp&nbsp██║&nbsp&nbsp&nbsp&nbsp&nbsp██║&nbsp&nbsp██║╚██████╔╝██║&nbsp&nbsp&nbsp&nbsp&nbsp██║███████╗███████╗</p>
+	<p contenteditable="false">╚═╝&nbsp&nbsp╚═╝╚═╝&nbsp&nbsp╚═══╝╚═╝&nbsp&nbsp╚═╝╚══════╝╚══════╝&nbsp&nbsp&nbsp&nbsp╚═╝&nbsp&nbsp&nbsp&nbsp&nbsp╚═╝&nbsp&nbsp╚═╝&nbsp╚═════╝&nbsp╚═╝&nbsp&nbsp&nbsp&nbsp&nbsp╚═╝╚══════╝╚══════╝</p>
+	<p contenteditable="false">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p>
 	<p contenteditable="false">Welcome to AnhLe's Page</p>
-	<p>${shellPrompt}</p>`
+	<p contenteditable="false"><br></p>
+	<p contenteditable="false" style="color:#3333ff;">CLI command:</p>
+	<p contenteditable="false">&nbsp&nbspinfo: Print Info</p>
+	<p contenteditable="false">&nbsp&nbspexp: Print Experience</p>
+	<p contenteditable="false">&nbsp&nbspedu: Print Education</p>
+	${shellPrompt}</p>`
 }
 
 
