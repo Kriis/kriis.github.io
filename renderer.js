f349bb7d73db57923d4287cca822751bfedfd439
Hai Anh Le <HaiAnh.Le@rockwellautomation.com>
2025-09-10 16:52:46 +0800

    add class

--- renderer.js
+++ renderer.js
@@ -1,38 +1,169 @@
 const container = document.getElementById('container')
 const shellPrompt = "<span style=\"display:inline; color:#5BE44C;\">></span>&nbsp;"
+const errorPrompt = `<p><span style="color:red";>&nbsp&nbspx</span> Error: Unknown command</p>`
 
-let terminal = document.createElement("div")
+function createNewPrompt(term) {
+	const p = document.createElement('p')
+	p.innerHTML = `${shellPrompt}`
+	term.appendChild(p)
+	putCursorAtEnd(term)
+}
 
-terminal.setAttribute("class", "terminal")
-terminal.setAttribute("contenteditable", "true")
-terminal.setAttribute("tabindex", "0")
+function putCursorAtEnd(term) {
+    let range,selection;
+    if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+    {
+        range = document.createRange();//Create a range (a range is a like the selection but invisible)
+        range.selectNodeContents(term);//Select the entire contents of the element with the range
+        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+        selection = window.getSelection();//get the selection object (allows you to change selection)
+        selection.removeAllRanges();//remove any selections already made
+        selection.addRange(range);//make the range you have just created the visible selection
+    }
+    else if(document.selection)//IE 8 and lower
+    { 
+        range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+        range.moveToElementText(term);//Select the entire contents of the element with the range
+        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+        range.select();//Select the range (make it the visible selection
+    }
+    term.scrollTop = term.scrollHeight
+}
 
+function printInfo(term) {
+	term.innerHTML += `
+	<p contenteditable="false">&nbsp&nbsp Senior Embedded Dev with 10+ years of experience</p>
+	<p contenteditable="false">&nbsp&nbsp I just love coding and embedded systems!!!!!!!!!!!!!!!</p>
+	<p contenteditable="false">&nbsp&nbsp Problem solver and quick adapter person.</p>
+	<p contenteditable="false">&nbsp&nbsp Things I do: Autosar, Hardware Secure Module(HSM), Bootloader, IoT, BLE, Linux, RTOS....</p>
+	`
+}
 
-function printBanner(terminal) {
-	terminal.innerHTML = `<div id="filterBar" style="display:none"></div>
-	<p contenteditable="false">&nbsp█████╗&nbsp███╗&nbsp&nbsp&nbsp██╗██╗&nbsp&nbsp██╗██╗&nbsp&nbsp&nbsp&nbsp&nbsp███████╗&nbsp&nbsp&nbsp&nbsp██████╗&nbsp██████╗&nbsp&nbsp██████╗&nbsp███████╗██╗██╗&nbsp&nbsp&nbsp&nbsp&nbsp███████╗</p>
-	<p contenteditable="false">██╔══██╗████╗&nbsp&nbsp██║██║&nbsp&nbsp██║██║&nbsp&nbsp&nbsp&nbsp&nbsp██╔════╝&nbsp&nbsp&nbsp&nbsp██╔══██╗██╔══██╗██╔═══██╗██╔════╝██║██║&nbsp&nbsp&nbsp&nbsp&nbsp██╔════╝</p>
-	<p contenteditable="false">███████║██╔██╗&nbsp██║███████║██║&nbsp&nbsp&nbsp&nbsp&nbsp█████╗&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp██████╔╝██████╔╝██║&nbsp&nbsp&nbsp██║█████╗&nbsp&nbsp██║██║&nbsp&nbsp&nbsp&nbsp&nbsp█████╗&nbsp&nbsp</p>
-	<p contenteditable="false">██╔══██║██║╚██╗██║██╔══██║██║&nbsp&nbsp&nbsp&nbsp&nbsp██╔══╝&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp██╔═══╝&nbsp██╔══██╗██║&nbsp&nbsp&nbsp██║██╔══╝&nbsp&nbsp██║██║&nbsp&nbsp&nbsp&nbsp&nbsp██╔══╝&nbsp&nbsp</p>
-	<p contenteditable="false">██║&nbsp&nbsp██║██║&nbsp╚████║██║&nbsp&nbsp██║███████╗███████╗&nbsp&nbsp&nbsp&nbsp██║&nbsp&nbsp&nbsp&nbsp&nbsp██║&nbsp&nbsp██║╚██████╔╝██║&nbsp&nbsp&nbsp&nbsp&nbsp██║███████╗███████╗</p>
-	<p contenteditable="false">╚═╝&nbsp&nbsp╚═╝╚═╝&nbsp&nbsp╚═══╝╚═╝&nbsp&nbsp╚═╝╚══════╝╚══════╝&nbsp&nbsp&nbsp&nbsp╚═╝&nbsp&nbsp&nbsp&nbsp&nbsp╚═╝&nbsp&nbsp╚═╝&nbsp╚═════╝&nbsp╚═╝&nbsp&nbsp&nbsp&nbsp&nbsp╚═╝╚══════╝╚══════╝</p>
-	<p contenteditable="false">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p>
-	<p contenteditable="false">Welcome to AnhLe's Page</p>
-	<p contenteditable="false"><br></p>
-	<p contenteditable="false" style="color:#3333ff;">CLI command:</p>
-	<p contenteditable="false">&nbsp&nbspinfo: Print Info</p>
-	<p contenteditable="false">&nbsp&nbspexp: Print Experience</p>
-	<p contenteditable="false">&nbsp&nbspedu: Print Education</p>
-	${shellPrompt}</p>`
+function printEdu(term) {
+	term.innerHTML += `
+	<p contenteditable="false">&nbsp&nbsp Computer Enginnering</p>
+	<p contenteditable="false">&nbsp&nbsp&nbsp&nbsp HCM University of Technology and Education</p>
+	`
 }
 
 
-function setFocus(terminal) {
-	terminal.focus()
+class Terminal {
+	constructor(location) {
+		this.location = location
+		this.div = document.createElement("div")
+		this.div.setAttribute("class", "terminal")
+		this.div.setAttribute("contenteditable", "true")
+		this.div.setAttribute("tabindex", "0")
+
+		this.div.commandPrompt = ""
+	}
+
+	printBanner() {
+		this.div.innerHTML = `<div id="filterBar" style="display:none"></div>
+		<p contenteditable="false">&nbsp█████╗&nbsp███╗&nbsp&nbsp&nbsp██╗██╗&nbsp&nbsp██╗██╗&nbsp&nbsp&nbsp&nbsp&nbsp███████╗&nbsp&nbsp&nbsp&nbsp██████╗&nbsp██████╗&nbsp&nbsp██████╗&nbsp███████╗██╗██╗&nbsp&nbsp&nbsp&nbsp&nbsp███████╗</p>
+		<p contenteditable="false">██╔══██╗████╗&nbsp&nbsp██║██║&nbsp&nbsp██║██║&nbsp&nbsp&nbsp&nbsp&nbsp██╔════╝&nbsp&nbsp&nbsp&nbsp██╔══██╗██╔══██╗██╔═══██╗██╔════╝██║██║&nbsp&nbsp&nbsp&nbsp&nbsp██╔════╝</p>
+		<p contenteditable="false">███████║██╔██╗&nbsp██║███████║██║&nbsp&nbsp&nbsp&nbsp&nbsp█████╗&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp██████╔╝██████╔╝██║&nbsp&nbsp&nbsp██║█████╗&nbsp&nbsp██║██║&nbsp&nbsp&nbsp&nbsp&nbsp█████╗&nbsp&nbsp</p>
+		<p contenteditable="false">██╔══██║██║╚██╗██║██╔══██║██║&nbsp&nbsp&nbsp&nbsp&nbsp██╔══╝&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp██╔═══╝&nbsp██╔══██╗██║&nbsp&nbsp&nbsp██║██╔══╝&nbsp&nbsp██║██║&nbsp&nbsp&nbsp&nbsp&nbsp██╔══╝&nbsp&nbsp</p>
+		<p contenteditable="false">██║&nbsp&nbsp██║██║&nbsp╚████║██║&nbsp&nbsp██║███████╗███████╗&nbsp&nbsp&nbsp&nbsp██║&nbsp&nbsp&nbsp&nbsp&nbsp██║&nbsp&nbsp██║╚██████╔╝██║&nbsp&nbsp&nbsp&nbsp&nbsp██║███████╗███████╗</p>
+		<p contenteditable="false">╚═╝&nbsp&nbsp╚═╝╚═╝&nbsp&nbsp╚═══╝╚═╝&nbsp&nbsp╚═╝╚══════╝╚══════╝&nbsp&nbsp&nbsp&nbsp╚═╝&nbsp&nbsp&nbsp&nbsp&nbsp╚═╝&nbsp&nbsp╚═╝&nbsp╚═════╝&nbsp╚═╝&nbsp&nbsp&nbsp&nbsp&nbsp╚═╝╚══════╝╚══════╝</p>
+		<p contenteditable="false">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p>
+		<p contenteditable="false">Welcome to AnhLe's Page</p>
+		<p contenteditable="false"><br></p>
+		<p contenteditable="false" style="color:#3333ff;">CLI command:</p>
+		<p contenteditable="false">&nbsp&nbspinfo: Print Info</p>
+		<p contenteditable="false">&nbsp&nbspexp: Print Experience</p>
+		<p contenteditable="false">&nbsp&nbspedu: Print Education</p>
+		<p>${shellPrompt}</p>`
+	}
+
+    onFocusHandler() {
+        //put cursor to end of line
+        let range,selection;
+        if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+        {
+            range = document.createRange();//Create a range (a range is a like the selection but invisible)
+            range.selectNodeContents(this);//Select the entire contents of the element with the range
+            range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+            selection = window.getSelection();//get the selection object (allows you to change selection)
+            selection.removeAllRanges();//remove any selections already made
+            selection.addRange(range);//make the range you have just created the visible selection
+        }
+        else if(document.selection)//IE 8 and lower
+        { 
+            range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+            range.moveToElementText(this);//Select the entire contents of the element with the range
+            range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+            range.select();//Select the range (make it the visible selection
+        }
+    }
+
+	setFocus() {
+		this.div.focus()
+	}
+
+
+	keydownCmdHandler(ev) {
+		let evt = ev
+		if(evt.key === 'Enter') {
+			evt.preventDefault()
+			let command = this.lastElementChild.innerHTML
+			this.lastElementChild.setAttribute("contenteditable", "false")
+			command = command.substring(shellPrompt.length + 3, )
+			console.log(command)
+			switch(command) {
+				case "info":
+					printInfo(this)
+					break
+				case "exp":
+					break
+				case "edu":
+					printEdu(this)
+					break
+				default:
+					const error = document.createElement('p');
+					error.setAttribute("contenteditable", "false")
+					error.innerHTML = errorPrompt
+					this.appendChild(error)
+					break
+
+			}
+			createNewPrompt(this)
+		}
+	}
+
+
+	putCursorAtEnd() {
+		let range,selection;
+		if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+		{
+			range = document.createRange();//Create a range (a range is a like the selection but invisible)
+			range.selectNodeContents(this);//Select the entire contents of the element with the range
+			range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+			selection = window.getSelection();//get the selection object (allows you to change selection)
+			selection.removeAllRanges();//remove any selections already made
+			selection.addRange(range);//make the range you have just created the visible selection
+		}
+		else if(document.selection)//IE 8 and lower
+		{ 
+			range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+			range.moveToElementText(this);//Select the entire contents of the element with the range
+			range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+			range.select();//Select the range (make it the visible selection
+		}
+		term.scrollTop = term.scrollHeight
+	}
 }
 
-printBanner(terminal)
 
-container.appendChild(terminal)
+function createNewTerm(location) {
+    term = new Terminal(location)
+    term.printBanner()
+    term.div.onkeydown = term.keydownCmdHandler
+    term.div.onfocus = term.onFocusHandler
+    container.appendChild(term.div)
+    term.setFocus()
+}
 
-setFocus(terminal)
+if(container.hasChildNodes() === false) {
+	createNewTerm("center")
+}
